d3be82b3261df0edcd512dff77acc77d57250d4d
mulin_huang <mulin_huang@edge-core.com>
2020-11-25 10:37:35 +0800

    JIRA-SONIC-1473: Remove config.group() attribute from is_loopback_name_valid() function.

--- config/main.py
+++ config/main.py
@@ -3672,7 +3672,6 @@ def naming_mode_alias():
     """Set CLI interface naming mode to ALIAS (Vendor port alias)"""
     set_interface_naming_mode('alias')
 
-@config.group()
 def is_loopback_name_valid(loopback_name):
     """Loopback name validation
     """
